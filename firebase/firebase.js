eae1c760c9d1f6d196e0e08f7b6e19e351680d86
Ayush Bhardwaj <ayush@lifeguru.app>
2024-01-09 21:46:30 +0530

    exported the firebase services

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,7 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCJA0Uy1Kn81T2Z2r4vMdPTAp4Fj9HToMo",
   authDomain: "fir-todo-app-17558.firebaseapp.com",
@@ -12,6 +13,7 @@ const firebaseConfig = {
   measurementId: "G-V3E4SD0NR5",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
