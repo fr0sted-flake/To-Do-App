77c5e4ed854e9df652f72cd809083df3e18195e1
Ayush Bhardwaj <ayush@lifeguru.app>
2024-01-10 00:01:16 +0530

    registration feature done

--- pages/register.js
+++ pages/register.js
@@ -3,111 +3,109 @@ import { FcGoogle } from "react-icons/fc";
 import { useState, useEffect } from "react";
 import { auth } from "../firebase/firebase";
 import {
-  createUserWithEmailAndPassword,
-  updateProfile,
-  GoogleAuthProvider,
-  signInWithPopup,
+    signInWithEmailAndPassword,
+    GoogleAuthProvider,
+    signInWithPopup,
 } from "firebase/auth";
+import { useRouter } from "next/router";
+import { useAuth } from "@/firebase/auth";
+import Link from "next/link";
+import Loader from "@/components/Loader";
 
 const Provider = new GoogleAuthProvider();
 
-const RegisterForm = () => {
-  const [username, setUsername] = useState(null);
-  const [email, setEmail] = useState(null);
-  const [password, setPassword] = useState(null);
+const LoginForm = () => {
+    const router = useRouter();
+    const [email, setEmail] = useState(null);
+    const [password, setPassword] = useState(null);
+    const { authUser, isLoading } = useAuth();
 
-  const singupHandler = async () => {
-    if (!email || !password || !username) return;
-    try {
-      const { user } = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      await updateProfile(auth.currentUser, {
-        displayName: username,
-      });
-      setAuthUser({
-        uid: user.uid,
-        email: user.email,
-        username,
-      });
-    } catch (error) {
-      console.error("An error occured", error);
-    }
-  };
+    useEffect(() => {
+        if (!isLoading && authUser) {
+            router.push("/");
+        }
+    }, [authUser, isLoading]);
 
-  const signInWithGoogle = async () => {
-    const user = await signInWithPopup(auth, Provider);
-    
-  };
+    const loginHandler = async () => {
+        if (!email || !password) return;
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
 
-  return (
-    <main className="flex lg:h-[100vh]">
-      <div className="w-full lg:w-[60%] p-8 md:p-14 flex items-center justify-center lg:justify-start">
-        <div className="p-8 w-[600px]">
-          <h1 className="text-6xl font-semibold">Sign Up</h1>
-          <p className="mt-6 ml-1">
-            Already have an account ?{" "}
-            <span className="underline hover:text-blue-400 cursor-pointer">
-              Login
-            </span>
-          </p>
+    const signInWithGoogle = async () => {
+        try {
+            await signInWithPopup(auth, Provider);
+        } catch (error) {
+            console.error("An error occured", error);
+        }
+    };
 
-          <div className="bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90 flex justify-center items-center gap-4 cursor-pointer group">
-            <FcGoogle size={22} />
-            <span className="font-medium text-black group-hover:text-white">
-              Login with Google
-            </span>
-          </div>
+    return isLoading || (!isLoading && !!authUser) ? (
+        <Loader />
+    ) : (
+        <main className="flex lg:h-[100vh]">
+            <div className="w-full lg:w-[60%] p-8 md:p-14 flex items-center justify-center lg:justify-start">
+                <div className="p-8 w-[600px]">
+                    <h1 className="text-6xl font-semibold">Login</h1>
+                    <p className="mt-6 ml-1">
+                        Don't have an account ?{" "}
+                        <Link
+                            href="/register"
+                            className="underline hover:text-blue-400 cursor-pointer"
+                        >
+                            Sign Up
+                        </Link>
+                    </p>
 
-          <form onSubmit={(e) => e.preventDefault()}>
-            <div className="mt-10 pl-1 flex flex-col">
-              <label>Name</label>
-              <input
-                type="text"
-                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
-                onChange={(e) => setUsername(e.target.value)}
-                required
-              />
+                    <div className="bg-black/[0.05] text-white w-full py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90 flex justify-center items-center gap-4 cursor-pointer group">
+                        <FcGoogle size={22} />
+                        <span
+                            className="font-medium text-black group-hover:text-white"
+                            onClick={signInWithGoogle}
+                        >
+                            Login with Google
+                        </span>
+                    </div>
+
+                    <form onSubmit={(e) => e.preventDefault()}>
+                        <div className="mt-10 pl-1 flex flex-col">
+                            <label>Email</label>
+                            <input
+                                type="email"
+                                name="email"
+                                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </div>
+                        <div className="mt-10 pl-1 flex flex-col">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                name="password"
+                                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </div>
+                        <button
+                            className="bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90"
+                            onClick={loginHandler}
+                        >
+                            Sign in
+                        </button>
+                    </form>
+                </div>
             </div>
-            <div className="mt-10 pl-1 flex flex-col">
-              <label>Email</label>
-              <input
-                type="email"
-                name="email"
-                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-            </div>
-            <div className="mt-10 pl-1 flex flex-col">
-              <label>Password</label>
-              <input
-                type="password"
-                name="password"
-                className="font-medium border-b border-black p-4 outline-0 focus-within:border-blue-400"
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-            </div>
-            <button
-              className="bg-black text-white w-44 py-4 mt-10 rounded-full transition-transform hover:bg-black/[0.8] active:scale-90"
-              onClick={singupHandler}
-            >
-              Sign Up
-            </button>
-          </form>
-        </div>
-      </div>
-      <div
-        className="w-[40%] bg-slate-400 bg-cover bg-right-top hidden lg:block"
-        style={{
-          backgroundImage: "url('/login-banner.jpg')",
-        }}
-      ></div>
-    </main>
-  );
+            <div
+                className="w-[40%] bg-slate-400 bg-cover bg-right-top hidden lg:block"
+                style={{
+                    backgroundImage: "url('/login-banner.jpg')",
+                }}
+            ></div>
+        </main>
+    );
 };
 
-export default RegisterForm;
+export default LoginForm;
