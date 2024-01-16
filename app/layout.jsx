02aae8af2dd558257b31498d734970c6dccfd527
Ayush Bhardwaj <ayush@lifeguru.app>
2024-01-16 20:51:58 +0530

    done

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <AuthUserProvider><Suspense fallback>{children}</Suspense></AuthUserProvider>
+        <AuthUserProvider>{children}</AuthUserProvider>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -98,9 +98,9 @@ export default function Home() {
     }
   };
 
-  return  !authUser ? (
+  return !authUser ? (
     <Loader />
-) : (
+  ) : (
     <main className="">
       <div
         className="bg-black text-white w-44 py-4 mt-10 rounded-lg transition-transform hover:bg-black/[0.8] active:scale-90 flex items-center justify-center gap-2 font-medium shadow-md fixed bottom-5 right-5 cursor-pointer"
